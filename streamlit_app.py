93504202b4458d8d88c1468e3b86f2456bbbd429
sai-appdev4ever <141609601+sai-appdev4ever@users.noreply.github.com>
2023-08-09 00:58:02 +0530

    Update streamlit_app.py
    try else added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLERROR
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -27,15 +27,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice Api
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(fruityvice_normalized)
+expect URLError as e:
+  streamlit.error()
 streamlit.stop()
 
 
